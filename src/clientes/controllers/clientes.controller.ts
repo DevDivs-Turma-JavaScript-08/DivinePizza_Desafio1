ab5c74b9c9ec442c3d7a5ccf434fcffa0e69560f
Grazielle Gualter <grazi.gualter@gmail.com>
2025-07-31 12:23:49 -0300

    feat: adiciona service

--- src/clientes/controllers/clientes.controller.ts
+++
@@ -1,1 +0,0 @@
-// Controller recebe e controla as requisições através de rotas (protocolos HTTP com endpoints e subendpoints(/rota/subrota)).
--- src/clientes/services/clientes.service.ts
+++ src/clientes/services/clientes.service.ts
@@ -1,2 +1,57 @@
-// Contém todos os métodos.
-// Principal característica dos services são suas regras.
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { Cliente } from "../entities/cliente.entity";
+import { DeleteResult, ILike, Repository } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+
+
+
+@Injectable()
+export class ClientesService {
+    contructor(
+        @InjectRepository(Cliente)
+        private clientesRepository: Repository<Cliente>
+
+    ) {}
+
+    async findByAll(): Promise<Cliente[]> {
+        return this.clientesRepository.find()
+    }
+
+    async findById(id: number): Promise<Cliente> {
+
+        const cliente = await this.clientesRepository.findOneBy({
+            where: { id }
+        });
+        if (!cliente) {
+            throw new HttpException("Cliente não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return cliente;
+    }
+
+    async findByNome(nome: string): Promise<Cliente[]> {
+        return this.clientesRepository.find({
+            where: { nome: ILike(`%${nome}%`) }
+        });
+    }
+
+    async create(cliente: Cliente): Promise<Cliente> {
+        return this.clientesRepository.save(cliente);
+    }
+
+    async update(cliente: Cliente): Promise<Cliente> {
+        const clienteExistente = await this.clientesRepository.findOneBy({ id: cliente.id });
+        if (!clienteExistente) {
+            throw new HttpException("Cliente não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return this.clientesRepository.save(cliente);
+    }
+
+    async delete(id: number): Promise<DeleteResult> {
+        const clienteExistente = await this.clientesRepository.findOneBy({ id });
+        if (!clienteExistente) {
+            throw new HttpException("Cliente não encontrado", HttpStatus.NOT_FOUND);
+        }
+        return this.clientesRepository.delete(id);
+    }
+
+}
