93e007bdb78db99d2602f08600067d69037f663b
Agataandrade <agatasouandrade@gmail.com>
2025-07-31 11:10:07 -0300

    feat: configura a main e app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([])],
   controllers: [AppController],
   providers: [AppService],
+  exports: []
 })
 export class AppModule {}
