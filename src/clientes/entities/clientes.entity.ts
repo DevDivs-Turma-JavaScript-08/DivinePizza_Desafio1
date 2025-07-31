3c0b114c0aed60a36c6f199d812f585a0a856cb0
Pedro Henrique <karpatech2025@gmail.com>
2025-07-31 12:04:22 -0300

    Feat: Adiciona Entity

--- src/clientes/entities/clientes.entity.ts
+++ src/clientes/entities/clientes.entity.ts
@@ -1,1 +1,33 @@
 // Define a tabela "tb_clientes".1
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty } from 'class-validator';
+
+@Entity({ name: 'tb_clientes' })
+export class Cliente {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  nome: string;
+
+  @IsNotEmpty()
+  @Column({ length: 1000, nullable: false })
+  endereco: string;
+
+  @IsNotEmpty()
+  @Column({ length: 100, nullable: false })
+  email: string;
+
+  @IsNotEmpty()
+  @Column({ nullable: false })
+  telefone: number;
+
+  @IsNotEmpty()
+  @Column({ nullable: false })
+  quantPedido: number;
+
+  @IsNotEmpty()
+  @Column({ nullable: false })
+  ultimoPedido: number;
+}
